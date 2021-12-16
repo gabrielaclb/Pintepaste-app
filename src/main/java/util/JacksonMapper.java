22abbf0d61b6a3d5cb9b564b4af30e9f6e3753e4
GabsDevs <gabrielaclb5@gmail.com>
2021-12-16 18:23:51 -0400

    Jackson methods uploaded
    I forgot to do it in the last commit xd

--- src/main/java/util/JacksonMapper.java
+++ src/main/java/util/JacksonMapper.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonMapper {
@@ -13,4 +14,8 @@ public class JacksonMapper {
 	public <T> T jsonToPojo(HttpServletRequest request, Class clase) throws IOException{
         return (T) objMap.readValue(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())), clase);
     }
+	
+	public <T> String pojoToJson(T data) throws JsonProcessingException {
+		return objMap.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+	}
 }
